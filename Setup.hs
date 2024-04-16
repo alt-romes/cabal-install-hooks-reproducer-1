bdda14ccfaf2df0893d82081725ccc046fa09470
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-04-16 17:39:40 +0100

    Try to invoke buildtool from within Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,7 @@
+import Data.Maybe
 import Distribution.Simple
 import Distribution.Simple.UserHooks
+import Distribution.Verbosity
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Program
 import Distribution.Simple.Program.Db
@@ -9,6 +11,10 @@ main = defaultMainWithHooks simpleUserHooks
   { buildHook = \pkg lbi hooks flags -> do
       let myProgName = "buildtool"
           myProg = simpleProgram myProgName
-      putStrLn $ myProgName ++ ": " ++ show (lookupProgram myProg (withPrograms lbi))
+          prog = lookupProgram myProg (withPrograms lbi)
+      putStrLn $ myProgName ++ ": " ++ show prog
+      putStrLn "Running prog..."
+      runProgram normal (fromJust prog) []
+      putStrLn "Done.!"
       buildHook simpleUserHooks pkg lbi hooks flags
   }
